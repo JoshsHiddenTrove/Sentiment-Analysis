2934fc3071049282d0738cccdf5d6b4a6bb58f4b
josh <56923405+JoshsHiddenTrove@users.noreply.github.com>
2021-01-20 00:32:16 -0700

    Create ParsingFunct.py

---
+++ ParsingFunct.py
@@ -0,0 +1,28 @@
+
+from nltk.corpus import stopwords
+
+english_stop_words = stopwords.words('english')
+def remove_stop_words(corpus):
+    removed_stop_words = []
+    for review in corpus:
+        removed_stop_words.append(
+            ' '.join([word for word in review.split() 
+                      if word not in english_stop_words])
+        )
+    return removed_stop_words
+
+no_stop_words = remove_stop_words(reviews_train_clean)
+
+def get_stemmed_text(corpus):
+    from nltk.stem.porter import PorterStemmer
+    stemmer = PorterStemmer()
+    return [' '.join([stemmer.stem(word) for word in review.split()]) for review in corpus]
+
+stemmed_reviews = get_stemmed_text(reviews_train_clean)
+
+def get_lemmatized_text(corpus):
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
+    return [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in corpus]
+
+lemmatized_reviews = get_lemmatized_text(reviews_train_clean)
